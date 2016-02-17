a3f7e61a95fe06c8883ad602c007f7b00badafde
speaking-fish <speakingfish@gmail.com>
2016-02-17 21:07:57 +0300

    1.5 compatibility

---
+++ src/com/speakingfish/common/map/AbstractMap.java
@@ -0,0 +1,15 @@
+package com.speakingfish.common.map;
+
+/**
+ * Compatibility stub
+ * 
+ * @param <K>
+ * @param <V>
+ */
+public abstract class AbstractMap<K,V> extends java.util.AbstractMap<K,V> {
+    
+    protected AbstractMap() {
+    }
+
+
+}
